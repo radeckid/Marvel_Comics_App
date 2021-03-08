124f8754d1d2430c9878e8f599aebec1dc272c2d
Damian Radecki <damianradecki@MacBook-Air-Damian.local>
2021-03-08 14:53:55 +0100

    Refactoring

--- app/src/main/java/pl/damrad/marvelcomicsapp/adapters/items/ComicsItem.kt
+++ app/src/main/java/pl/damrad/marvelcomicsapp/adapters/items/ComicsItem.kt
@@ -1,7 +1,7 @@
 package pl.damrad.marvelcomicsapp.adapters.items
 
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 class ComicsItem(
