e092f2144a8644a208c30263cade9bf46b449ade
Damian Radecki <damianradecki@MacBook-Air-Damian.local>
2021-03-11 11:05:30 +0100

    Check network connection - show snackbar

--- app/src/main/java/pl/damrad/marvelcomicsapp/fragments/SearchFragment.kt
+++ app/src/main/java/pl/damrad/marvelcomicsapp/fragments/SearchFragment.kt
@@ -1,6 +1,11 @@
 package pl.damrad.marvelcomicsapp.fragments
 
+import android.content.Context
 import android.graphics.Color
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -46,7 +51,7 @@ class SearchFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         connectionBar = createSnackbar(view)
-
+        checkNetwork()
         initRecyclerView()
         setSearchView()
         setObservers()
@@ -134,5 +139,32 @@ class SearchFragment : Fragment() {
             .setActionTextColor(Color.WHITE)
             .setBackgroundTint(Color.RED)
             .setTextColor(Color.WHITE)
+    }
+
+    private fun checkNetwork() {
+        val networkRequest = NetworkRequest.Builder()
+            .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+            .addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR)
+            .build()
+
+        val cm = context?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        cm.registerNetworkCallback(networkRequest, object : ConnectivityManager.NetworkCallback() {
+            override fun onAvailable(network: Network) {
+                super.onAvailable(network)
+                mainViewModel.connectionState.postValue(NetworkState.Connected)
+
+                mainViewModel.getAllComics()
+            }
+
+            override fun onUnavailable() {
+                super.onUnavailable()
+                mainViewModel.connectionState.postValue(NetworkState.Disconnected)
+            }
+
+            override fun onLost(network: Network) {
+                super.onLost(network)
+                mainViewModel.connectionState.postValue(NetworkState.Disconnected)
+            }
+        })
     }
 }
